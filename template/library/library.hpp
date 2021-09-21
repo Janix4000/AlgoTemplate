39fc540e699660eebfdfb1c37b15b8e1615b1d25
janix4000 <janix4000@gmail.com>
2021-09-21 23:53:27 +0200

    clear library a little bit

--- template/library/library.hpp
+++ template/library/library.hpp
@@ -1,121 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Vec2
-{
-    int x = 0, y = 0;
-    Vec2(int x, int y) : x(x), y(y) {}
-    Vec2() = default;
-    Vec2 &operator+=(const Vec2 &rhs)
-    {
-        x += rhs.x;
-        y += rhs.y;
-        return *this;
-    }
-    Vec2 operator-() const
-    {
-        return *this * -1;
-    }
-    Vec2 operator+(const Vec2 &rhs) const
-    {
-        auto res = *this;
-        res += rhs;
-        return res;
-    }
-    Vec2 &operator-=(const Vec2 &rhs)
-    {
-        return (*this += -rhs);
-    }
-    Vec2 operator-(const Vec2 &rhs) const
-    {
-        return *this + (-rhs);
-    }
-    Vec2 &operator*=(int rhs)
-    {
-        x *= rhs;
-        y *= rhs;
-        return *this;
-    }
-    Vec2 operator*(int rhs) const
-    {
-        auto res = *this;
-        res *= rhs;
-        return res;
-    }
-};
-
-using It = string::iterator;
-std::vector<int> get_kmp(It beg, It end)
-{
-    const size_t size = end - beg;
-    std::vector<int> tab(size + 1);
-    tab[0] = -1;
-    int k = -1;
-
-    for (size_t i = 0; i < size; ++i)
-    {
-        while (k != -1 && beg[k] != beg[i])
-            k = tab[k];
-        tab[i + 1] = ++k;
-    }
-    return tab;
-}
-
-template <typename T, size_t N>
-class Vector : public array<T, N>
-{
-    Vector &operator+=(const Vector<T, N> &rhs)
-    {
-        transform(rhs.begin(), rhs.end(), this->begin(), this->begin(), plus<T>{});
-        return *this;
-    }
-    Vector operator-() const
-    {
-        return *this * -1;
-    }
-    Vector operator+(const Vector<T, N> &rhs) const
-    {
-        auto res = *this;
-        res += rhs;
-        return res;
-    }
-    Vector &operator-=(const Vector<T, N> &rhs)
-    {
-        return (*this += -rhs);
-    }
-    Vector operator-(const Vector<T, N> &rhs) const
-    {
-        return *this + (-rhs);
-    }
-    Vector &operator*=(int rhs)
-    {
-        transform(rhs.begin(), rhs.end(), this->begin(), this->begin(), multiplies<T>{});
-        return *this;
-    }
-    Vector operator*(int rhs) const
-    {
-        auto res = *this;
-        res *= rhs;
-        return res;
-    }
-};
-
-using ll = long long;
-constexpr ll P = 1000000007;
-
-ll power(ll a, ll n, const ll P = P)
-{
-    ll res = 1;
-    while (n)
-    {
-        if (n % 2)
-        {
-            res = (res * a) % P;
-        }
-        n /= 2;
-        a = (a * a) % P;
-    }
-    return res;
-}
 
 template <class T, size_t N>
 array<T, N> make_array(const T &v)
@@ -124,7 +8,6 @@ array<T, N> make_array(const T &v)
     ret.fill(v);
     return ret;
 }
-
 class AhoCorasic
 {
     static constexpr int N_A = 'z' - 'a' + 1;
