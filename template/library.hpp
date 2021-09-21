e5ec9691bbd26107dd9584b4ba8ae64544ddc35a
janix4000 <janix4000@gmail.com>
2021-09-21 20:30:31 +0200

    add better graph

--- template/library.hpp
+++ template/library.hpp
@@ -60,7 +60,58 @@ std::vector<int> get_kmp(It beg, It end)
     return tab;
 }
 
-using Graph = vector<vector<int>>;
+template <class T>
+struct Edge
+{
+    Edge(int dest, const T &value = 1) : dest(dest), value(value) {}
+    int dest;
+    T value;
+    operator int() const { return dest; }
+};
+
+template <class T>
+struct Node
+{
+    Node() = default;
+    Node(const initializer_list<int> &destinations)
+    {
+        edges.reserve(destinations.size());
+        for (int dest : destinations)
+        {
+            edges.emplace_back(dest);
+        }
+    }
+    vector<Edge<T>> edges;
+    bool visited;
+    int parent;
+    void push_back(int dest, int value = 1) { edges.emplace_back(dest, value); }
+    Edge<T> &operator[](size_t idx) { return edges[idx]; }
+    auto begin() { return edges.begin(); }
+    auto end() { return edges.end(); }
+};
+
+template <class T>
+using GraphT = vector<Node<T>>;
+
+using Graph = GraphT<int>;
+
+template <typename T>
+void ___print(const Edge<T> &edge)
+{
+    ___print(make_pair(edge.dest, edge.value));
+}
+template <typename T>
+void ___print(const Node<T> &node)
+{
+    ___print(node.edges);
+}
+template <typename T>
+void ___print(const GraphT<T> &graph)
+{
+    int f = 0;
+    for (auto &node : graph)
+        cerr << (f++ ? "\n" : "") << f - 1 << ": ", ___print(node);
+}
 
 bool dfs(Graph &graph, int v = 0)
 {
