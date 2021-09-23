99f4a0d538926f7108612d30016ddac45a7715f4
janix4000 <janix4000@gmail.com>
2021-09-23 09:00:44 +0200

    add gcd

---
+++ template/library/number_theory/extended_gcd.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct extended_gcd
+{
+    ll gcd, x, y;
+    extended_gcd(ll a, ll b)
+    {
+        gcd = __get_gcd(a, b, x, y);
+    }
+
+private:
+    ll __get_gcd(ll a, ll b, ll &x, ll &y)
+    {
+        if (b == 0)
+        {
+            x = 1;
+            y = 0;
+            return a;
+        }
+        ll x1, y1;
+        ll gcd = __get_gcd(b, a % b, x1, y1);
+        x = y1;
+        y = x1 - y1 * (a / b);
+        return gcd;
+    }
+};
---
+++ template/library/number_theory/gcd.hpp
@@ -0,0 +1,4 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+inline T get_gcd(const T a, const T b) { return __gcd(a, b); }
