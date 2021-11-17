df741917d642cdc4b0c7fdb4283946b8789f7c34
janix4000 <janix4000@gmail.com>
2021-11-17 17:58:21 +0100

    remove rarely used and commented code

--- template/template.cpp
+++ template/template.cpp
@@ -8,23 +8,6 @@ void __print(Ts &&...ts) {}
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-typedef complex<ld> cd;
-
-// typedef pair<int, int> pi;
-// typedef pair<ll, ll> pl;
-// typedef pair<ld, ld> pd;
-
-// template <typename T>
-// using vec = vector<T>;
-// template <typename T>
-// using arr = array<T>;
-// typedef vector<int> vi;
-// typedef vector<vi> vvi;
-// typedef vector<ll> vl;
-// typedef vector<vl> vvl;
-
 template <class T>
 using pq_max = priority_queue<T>;
 template <class T>
@@ -32,26 +15,9 @@ using pq_min = priority_queue<T, vector<T>, greater<T>>;
 
 #define FOR(i, a, b) for (int i = a; i < (b); ++i)
 #define F0R(i, a) for (int i = 0; i < (a); ++i)
-// #define loop(i, b) for (int i = 0; i < (b); ++i)
-// #define loopr(i, b) for (int i = (b)-1; i >= 0; --i)
 #define FORd(i, a, b) for (int i = (b)-1; i >= a; --i)
 #define F0Rd(i, a) for (int i = (a)-1; i >= 0; --i)
-#define trav(a, x) for (auto &a : x)
 #define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
-
-#define IN(n) \
-    int n;    \
-    cin >> n;
-
-// #define sz(x) (int)(x).size()
-// #define mp make_pair
-// #define pb push_back
-// #define fir first
-// #define sec second
-// #define lb lower_bound
-// #define ub upper_bound
-// #define ins insert
-// #define beg begin
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
