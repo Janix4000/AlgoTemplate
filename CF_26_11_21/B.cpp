5babff0163aa3ccb9ada16d15d310c9c598e7b2e
Janix4000 <janix4000@gmail.com>
2021-11-27 15:35:56 +0100

    back

---
+++ CF_26_11_21/B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+template <typename... Ts>
+void __print(Ts &&...ts) {}
+#ifdef DEBUG
+#include "print.hpp"
+#endif  // DEBUG
+
+using namespace std;
+
+template <class T>
+using pq_max = priority_queue<T>;
+template <class T>
+using pq_min = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i = a; i < (b); ++i)
+#define F0R(i, a) for (int i = 0; i < (a); ++i)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; --i)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; --i)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x) x.begin(), x.end()
+#define loop(i, b) for (int i = 0; i < (b); ++i)
+#define loopr(i, b) for (int i = (b)-1; i >= 0; --i)
+#define sz(x) ((int)(x).size())
+
+using ll = long long;
+template <typename T>
+using vec = vector<T>;
+template <typename T, size_t N>
+using arr = array<T, N>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#define int ll
+
+class Solution {};
+
+void solution() {
+    int n;
+    cin >> n;
+    vi as(n);
+    loop(i, n) { cin >> as[i]; }
+    vi is(n);
+    iota(all(is), 0);
+    sort(all(is), [&](int a, int b) { return as[a] > as[b]; });
+    // sort(all(as), greater<int>{});
+    int i = 2;
+    int T = 0;
+    vi res(n);
+    for (int idx : is) {
+        int a = as[idx];
+        res[idx] = (i / 2) * ((i % 2) * 2 - 1);
+        T += 2 * a * (i / 2);
+        i++;
+    }
+    cout << T << '\n';
+    cout << "0 ";
+    for (int r : res) {
+        cout << r << ' ';
+    }
+    cout << '\n';
+}
+#undef int
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int qs = 1;
+    cin >> qs;
+
+    while (qs--) {
+        solution();
+    }
+
+    // Solution solution;
+    return 0;
+}
